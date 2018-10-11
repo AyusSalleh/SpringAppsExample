95acdc3ce0a04c86ee0874c70a37e90cd6eb0d50
Winapp Developer <ayus.salleh95@gmail.com>
2018-10-11 19:23:06 +0800

    Integrate with PosgreSQL

---
+++ src/main/java/com/example/myapp/model/Question.java
@@ -0,0 +1,60 @@
+package com.example.myapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    // Getters and Setters (Omitted for brevity)
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return title;
+	}
+
+	public void setTitle(String getTitle) {
+		// TODO Auto-generated method stub
+		this.title = getTitle;
+	}
+
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return description;
+	}
+
+	public void setDescription(String getDesc) {
+		// TODO Auto-generated method stub
+		this.description = getDesc;
+	}
+	
+//    public String getName() {
+//        return name;
+//     }  
+//     public void setName(String name) {
+//        this.name = name;
+//     }
+//     public int getId() {
+//        return id;
+//     }   
+//     public void setId(int id) {
+//        this.id = id;
+//     }
+
+}
---
+++ src/main/java/com/example/myapp/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.example.myapp.repository;
+
+import com.example.myapp.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
